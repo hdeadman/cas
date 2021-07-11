36bc68f3692c6ef43267c6d4ac020c820aa26d50
Hal Deadman <hal.deadman@gmail.com>
2021-07-10 22:34:28 -0400

    add puppeteer test with real oidc app

--- ci/tests/puppeteer/scenarios/external-app-strapi-oidc/init.sh
+++ ci/tests/puppeteer/scenarios/external-app-strapi-oidc/init.sh
@@ -27,24 +27,25 @@ cp $STRAPI_FOLDER/$PROJECT/node_modules/strapi-plugin-users-permissions/config/u
 # run without SSL verification in order to call CAS via https and not worry about SSL trust
 NODE_TLS_REJECT_UNAUTHORIZED=0 yarn develop &
 pid=$!
-if [[ "$CI" != "true" ]]; then
-  fg 1
-else
-  echo "Waiting for Strapi start up"
-  until curl -k -L --output /dev/null --silent --fail http://localhost:1337; do
-    echo -n '.'
-    sleep 1
-  done
-  echo "Strapi Ready - PID: $pid"
-fi
+echo "Waiting for Strapi start up in background"
+until curl -k -L --output /dev/null --silent --fail http://localhost:1337; do
+  echo -n '.'
+  sleep 1
+done
+echo "Strapi Ready - PID: $pid"
 
 if [[ "$SKIP_REGISTRATION" != "true" ]]; then
   # register strapi administrator user
   DATA='{"email":"admin@example.org","password":"P@ssw0rd","firstname":"Strapi","lastname":"Admin"}'
   echo $DATA > .admin.txt
   cat .admin.txt
+  # allow error in case already registered
+  set +e
   curl -X POST -H "Content-Type: application/json" http://localhost:1337/admin/register-admin --data @./.admin.txt
   rm .admin.txt
 fi
+
+# let strapi keep running
+disown -h %1
 
 cd -
